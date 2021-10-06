c86423d2a08ca069db4f5d460c91bfd75de07171
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-10-06 20:29:08 +0300

    Treat local names in the search results as a nullable field

--- app/src/main/java/me/alexpetrakov/cyclone/locationsearch/data/openweathermap/SearchResultsJson.kt
+++ app/src/main/java/me/alexpetrakov/cyclone/locationsearch/data/openweathermap/SearchResultsJson.kt
@@ -9,7 +9,7 @@ import java.util.*
 @JsonClass(generateAdapter = true)
 data class SearchResultJson(
     @Json(name = "name") val internationalName: String,
-    @Json(name = "local_names") val languagesToLocalNames: Map<String, String>,
+    @Json(name = "local_names") val languagesToLocalNames: Map<String, String>?,
     @Json(name = "lat") val lat: Double,
     @Json(name = "lon") val lon: Double,
     @Json(name = "country") val country: String,
@@ -17,7 +17,7 @@ data class SearchResultJson(
 )
 
 fun SearchResultJson.toDomain(locale: Locale): SearchResult {
-    val placeName = languagesToLocalNames[locale.language] ?: internationalName
+    val placeName = languagesToLocalNames?.get(locale.language) ?: internationalName
     val countryName = if (state != null) {
         "$country, $state"
     } else {
